457b6b41f19db5a4eafee8fbdf9b079e25351663
Antonio <antonio_ribeiro-n@estudante.sesisenai.org.br>
2026-04-06 22:42:15 -0300

    Added variables and graphic result

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.getElementById("formulario").addEventListener("submit")
+document.getElementById("formulario").addEventListener("submit", function(e) {
+
+e.preventDefault()
+
+let nome = document.getElementById("nome").value
+let email = document.getElementById("email").value
+let idade = document.getElementById("idade").value
+let peso = document.getElementById("peso").value
+let telefone = document.getElementById("telefone").value
+let cpf = document.getElementById("cpf").value
+let cidade = document.getElementById("cidade").value
+let estado = document.getElementById("estado").value
+let tipo = document.querySelector('input[name="tipo"]:checked')?.value
+
+
+
+ let cadastro = [nome, email, idade, peso, telefone, cpf, cidade, estado, tipo];
+
+  document.getElementById("result").innerHTML =
+    'dados enviados:' +
+     "<pre>" + JSON.stringify(cadastro, null, 2) + "</pre>";
+
+
+})
